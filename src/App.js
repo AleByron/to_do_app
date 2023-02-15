d860de63c2ef0fd95e517d7d72f3a89f66939d75
Byron <alessandro.irertnuc@hotmail.it>
2023-02-15 17:16:35 +0100

    commit cambio pc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+   function toggleTaskCompleted(id){
+    const updateTasks = tasks.map((task) => {
+      if(task.id === id && task.completed === false){
+        task.completed = true;
+      }
+      else if(task.id === id && task.completed === true ){
+        task.completed = false;
+      }
+      return task;
+    })
+    setTasks(updateTasks);
+   }
+
+   function editTask(id){
+    const updateTasks = tasks.filter((task) => {
+      if (task.id === id){
+        task.name = props.name
+      }
+      return task;
+    })
+    setTasks(updateTasks);
+   }
+
+   function deleteTask(id){
+    const updateTasks = tasks.filter((task) => id !== task.id)
+    setTasks(updateTasks);
+   }
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+  const [isEditing, setEditing] = useState(false);
+
+  
+
+  let headingTextNoun = function Noun(taskList){
+    if (taskList.length > 1){
+      headingTextNoun = "tasks"
+    }
+    else {
+      headingTextNoun = "task"
+    }
+    return headingTextNoun;
+  }
+  const headingText = `${taskList.length} ${headingTextNoun(taskList)} remaining`;
+
+  function addTask(name){ //to put inside brackets at line 17
+    let newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    console.log(newTask);
+    //tasks.push(newTask);
+    setTasks([...tasks, newTask]);
+    console.log(tasks);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>ToDo App</h1>
+
+<Form addTaskProp={addTask} />
+      <div className="filters btn-group stack-exception">
+      <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+      {headingText} 
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+      {taskList}
+      </ul>
     </div>
   );
 }
+
 
 export default App;
